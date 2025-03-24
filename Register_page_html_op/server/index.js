b728e92d22ca9f8f4826d6a22ef79e1d091349c2
vmvijay05 <v.m.vijaykumar97@gmail.com>
2025-03-24 11:52:55 +0530

    minor_changes

--- Register_page_html_op/server/index.js
+++ Register_page_html_op/server/index.js
@@ -18,20 +18,21 @@ const server = http.createServer((req, res) => {
             }
         });
     }
-    // else if (req.url === "/" || req.url === "/index.html") {
-    //     // Serve the index.html file
-    //     const filePath = path.join(__dirname, 'view', 'index.html');
+    else if (req.url === "/" || req.url === "/index.html") {
+        // Serve the index.html file
+        const filePath = path.join(__dirname, 'view', 'index.html');
 
-    //     fs.readFile(filePath, (err, data) => {
-    //         if (err) {
-    //             res.writeHead(500, { 'Content-Type': 'text/plain' });
-    //             res.end("500 Internal Server Error");
-    //         } else {
-    //             res.writeHead(200, { 'Content-Type': 'text/html' });
-    //             res.end(data);
-    //         }
-    //     });
- else {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end("500 Internal Server Error");
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+    else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end("404 Not Found");
     }
